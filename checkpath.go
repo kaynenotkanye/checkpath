1a42f36d080aa88285b64e8bd32120b57eb80c80
Kayne Amornvivat <kaynenotkanye@outlook.com>
2018-09-02 09:19:20 -0700

    If else block for panic

--- checkpath.go
+++ checkpath.go
@@ -1,28 +1,27 @@
 package main
 
 import (
-	"os"
+	"flag"
 	"io"
 	"log"
-	"flag"
+	"os"
 )
 
 func main() {
 	osPath := flag.String("p", "", "Path or file to check")
 	flag.Parse()
 
-	logFile, err := os.OpenFile("checkpath.log", os.O_CREATE | os.O_APPEND | os.O_RDWR, 0666)
+	logFile, err := os.OpenFile("checkpath.log", os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
 	if err != nil {
 		panic(err)
 	}
 	mw := io.MultiWriter(os.Stdout, logFile)
 	log.SetOutput(mw)
 
-	if _, err := os.Stat(*osPath); os.IsNotExist(err){
-		log.Println("ERROR: The path does not exist.", err)
-	}
-
 	if _, err := os.Stat(*osPath); err == nil {
 		log.Println("The path exists: ", *osPath)
+	} else {
+		log.Println("ERROR: The path does not exist.", err)
+		panic(err)
 	}
 }
